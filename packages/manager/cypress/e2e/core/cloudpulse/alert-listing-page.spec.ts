d82c91a0d5af9caa0d03fd7d721d18ad7ae84003
agorthi <agorthi@akamai.com>
2025-01-20 19:28:21 +0530

    Add test cases for listing page

--- packages/manager/cypress/e2e/core/cloudpulse/alert-listing-page.spec.ts
+++ packages/manager/cypress/e2e/core/cloudpulse/alert-listing-page.spec.ts
@@ -1,7 +1,9 @@
 /**
  * @file Integration Tests for the CloudPulse DBaaS Alerts Listing Page.
- *
+ * This file contains integration tests to verify the functionality and UI elements
+ * on the CloudPulse DBaaS Alerts Listing Page.
  */
+
 import { mockAppendFeatureFlags } from 'support/intercepts/feature-flags';
 import { accountFactory, alertFactory } from 'src/factories';
 import { mockGetAccount } from 'support/intercepts/account';
@@ -10,47 +12,59 @@ import type { Flags } from 'src/featureFlags';
 import { mockGetAllAlertDefinitions } from 'support/intercepts/cloudpulse';
 import { randomDate } from 'src/utilities/random';
 import { formatDate } from 'src/utilities/formatDate';
+import { Alert } from '@linode/api-v4';
 import { ui } from 'support/ui';
-import { button } from 'support/ui/buttons';
 
+// Define feature flags to enable necessary features for testing
 const flags: Partial<Flags> = { aclp: { enabled: true, beta: true } };
 
+// Create a mock account for testing
 const mockAccount = accountFactory.build();
 const now = new Date();
 const tenDaysAgo = new Date(now.getTime() - 10 * 24 * 60 * 60 * 1000);
+const expectedHeaders = [
+  'Alert Name',
+  'Service',
+  'Status',
+  'Last Modified',
+  'Created By',
+];
 
-const mockAlertDbaas = alertFactory.build({
-  service_type: 'dbaas',
-  severity: 1,
-  status: 'enabled',
-  type: 'system',
-  entity_ids: ['1', '2'],
-  updated:randomDate(tenDaysAgo, now).toISOString(),
-});
-const mockAlertDbaasExtended = alertFactory.build({
-  service_type: 'dbaas',
-  severity: 0,
-  status: 'disabled',
-  type: 'custom',
-  entity_ids: ['1', '2'],
-  updated:randomDate(tenDaysAgo, now).toISOString(),
-});
-const mockAlertLinode = alertFactory.build({
-  service_type: 'linode',
-  severity: 2,
-  status: 'enabled',
-  type: 'default',
-  entity_ids: ['3', '4'],
-  updated:randomDate(tenDaysAgo, now).toISOString(),
-});
-const  mockAlertLinodeExtended = alertFactory.build({
-  service_type: 'linode',
-  severity: 3,
-  status: 'disabled',
-  type: 'user',
-  entity_ids: ['3', '4'],
-  updated:randomDate(tenDaysAgo, now).toISOString(),
-});
+// Generate mock alerts for testing
+const mockAlerts = [
+  alertFactory.build({
+    service_type: 'dbaas',
+    severity: 1,
+    status: 'enabled',
+    type: 'system',
+    entity_ids: ['1', '2'],
+    updated: randomDate(tenDaysAgo, now).toISOString(),
+  }),
+  alertFactory.build({
+    service_type: 'dbaas',
+    severity: 0,
+    status: 'disabled',
+    type: 'custom',
+    entity_ids: ['1', '2'],
+    updated: randomDate(tenDaysAgo, now).toISOString(),
+  }),
+  alertFactory.build({
+    service_type: 'linode',
+    severity: 2,
+    status: 'enabled',
+    type: 'default',
+    entity_ids: ['3', '4'],
+    updated: randomDate(tenDaysAgo, now).toISOString(),
+  }),
+  alertFactory.build({
+    service_type: 'linode',
+    severity: 3,
+    status: 'disabled',
+    type: 'user',
+    entity_ids: ['3', '4'],
+    updated: randomDate(tenDaysAgo, now).toISOString(),
+  }),
+];
 
 /**
  * Integration tests for the CloudPulse DBaaS Alerts Listing Page
@@ -58,175 +72,91 @@ const  mockAlertLinodeExtended = alertFactory.build({
 
 describe('Integration Tests for Dbaas Alert Listing Page', () => {
   beforeEach(() => {
+    // Set up feature flags and mock account before each test
     mockAppendFeatureFlags(flags);
     mockGetAccount(mockAccount);
 
-    mockGetAllAlertDefinitions([mockAlertDbaas,mockAlertLinode,mockAlertDbaasExtended,mockAlertLinodeExtended]).as('getAlertDefinitionsList');
-    
+    // Mock API response for alert definitions
+    mockGetAllAlertDefinitions(mockAlerts).as('getAlertDefinitionsList');
 
+    // Visit the alerts definitions page with login
     cy.visitWithLogin('/monitor/alerts/definitions');
 
+    // Wait for the mock API response
     cy.wait('@getAlertDefinitionsList');
-
   });
 
-  it('Lisiting page validation', () => {
-
-    // check avalible buton in listing page
-
-    // ui.button.findByTitle("Create").should("be.visible");
-
-      // Verify that the listing displays the DBaaS service type with the status set to 'Enabled'.
- 
-    cy.get(`[data-qa-alert-cell="${mockAlertDbaas.id}"]`).within(() => {
-      // Assert each field inside the alert cell
-        cy.findByText(mockAlertDbaas.service_type)
-          .should('be.visible')
-          .should('have.text',mockAlertDbaas.service_type)
-
-        cy.findByText(new RegExp(mockAlertDbaas.status, 'i'))
+  /**
+   * Function to check the details of an alert.
+   * @param {Alert} alert - The alert object to check.
+   */
+  const checkAlertDetails = (alert: Alert) => {
+    cy.get(`[data-qa-alert-cell="${alert.id}"]`).within(() => {
+      cy.findByText(alert.service_type)
         .should('be.visible')
-        .should('have.text','Enabled')
-        cy.findByText(mockAlertDbaas.label)
-          .should('be.visible');
-        cy.findByText(formatDate(mockAlertDbaas.updated, { format: 'yyyy-MM-dd HH:mm' }))
-         .should('be.visible')
-         .should('have.text',formatDate(mockAlertDbaas.updated, { format: 'yyyy-MM-dd HH:mm' }))
-
-        cy.findByText(mockAlertDbaas.created_by)
+        .should('have.text', alert.service_type);
+      cy.findByText(new RegExp(alert.status, 'i'))
         .should('be.visible')
-        .should('have.text',mockAlertDbaas.created_by)
-   
-        // Click the action button
-        cy.get(`[data-qa-alert-action-cell="alert_${mockAlertDbaas.id}"]`)
-          .find('button')
-         .should('be.visible')
-         .click({ force: true });
- 
-      });
-   
-          // Verify that the listing displays the Linode service type with the status set to 'Enabled'.
-
-      // Assert "Show Details" is visible
-      cy.get('[data-qa-action-menu-item="Show Details"]')
+        .should(
+          'have.text',
+          alert.status === 'enabled' ? 'Enabled' : 'Disabled'
+        );
+      cy.findByText(alert.label).should('be.visible');
+      cy.findByText(formatDate(alert.updated, { format: 'yyyy-MM-dd HH:mm' }))
         .should('be.visible')
- 
-        cy.get(`[data-qa-alert-action-cell="alert_${mockAlertDbaas.id}"]`)
+        .should(
+          'have.text',
+          formatDate(alert.updated, { format: 'yyyy-MM-dd HH:mm' })
+        );
+      cy.findByText(alert.created_by)
+        .should('be.visible')
+        .should('have.text', alert.created_by);
+      cy.get(`[data-qa-alert-action-cell="alert_${alert.id}"]`)
         .find('button')
-       .should('be.visible')
-       .click({ force: true });
+        .should('be.visible')
+        .click({ force: true });
+    });
+  };
+
+  it('Listing page validation', () => {
+    // Check if the "Create" button is visible on the page
+    cy.get('button[data-testid="button"]')
+      .contains('Create')
+      .should('be.visible');
+
+    // sorting using headers of the listing page
+    cy.get('[data-qa-sorting="alertName"]')
+      .should('be.visible')
+      .should('have.text', 'Alert Name');
+
+    cy.get('[data-qa-sorting="service"]')
+      .should('be.visible')
+      .should('have.text', 'Service');
+
+    cy.get('[data-qa-sorting="status"]')
+      .should('be.visible')
+      .should('have.text', 'Status');
+
+    cy.get('[data-qa-sorting="lastModified"]')
+      .should('be.visible')
+      .should('have.text', 'Last Modified');
+
+    cy.get('[data-qa-sorting="createdBy"]')
+      .should('be.visible')
+      .should('have.text', 'Created By');
 
 
-       cy.get(`[data-qa-alert-cell="${mockAlertLinode.id}"]`).within(() => {
-        // Assert each field inside the alert cell
-          cy.findByText(mockAlertLinode.service_type)
-            .should('be.visible')
-            .should('have.text',mockAlertLinode.service_type)
-  
-          cy.findByText(new RegExp(mockAlertLinode.status, 'i'))
-          .should('be.visible')
-          .should('have.text','Enabled')
-          cy.findByText(mockAlertLinode.label)
-            .should('be.visible');
-          cy.findByText(formatDate(mockAlertLinode.updated, { format: 'yyyy-MM-dd HH:mm' }))
-           .should('be.visible')
-           .should('have.text',formatDate(mockAlertLinode.updated, { format: 'yyyy-MM-dd HH:mm' }))
-  
-          cy.findByText(mockAlertLinode.created_by)
-          .should('be.visible')
-          .should('have.text',mockAlertLinode.created_by)
-     
-          // Click the action button
-          cy.get(`[data-qa-alert-action-cell="alert_${mockAlertLinode.id}"]`)
-            .find('button')
-           .should('be.visible')
-           .click({ force: true });
-   
-        });
-     
-        // Assert "Show Details" is visible
-        cy.get('[data-qa-action-menu-item="Show Details"]')
-          .should('be.visible')
-   
-          cy.get(`[data-qa-alert-action-cell="alert_${mockAlertLinode.id}"]`)
-          .find('button')
-         .should('be.visible')
-         .click({ force: true });
+    // validating Headers of the listing page
+    cy.get('[data-qa="alert-table"]').within(() => {
+      expectedHeaders.forEach((headerText) => {
+        cy.findByText(headerText).should('have.text', headerText);
+      });
+    });
 
-    
-    // Verify that the listing displays the DBaaS service type with the status set to 'Disabled'.
- 
-    cy.get(`[data-qa-alert-cell="${mockAlertDbaasExtended.id}"]`).within(() => {
-        // Assert each field inside the alert cell
-          cy.findByText(mockAlertDbaasExtended.service_type)
-            .should('be.visible')
-            .should('have.text',mockAlertDbaasExtended.service_type)
-  
-          cy.findByText(new RegExp(mockAlertDbaasExtended.status, 'i'))
-          .should('be.visible')
-          .should('have.text','Disabled')
-          cy.findByText(mockAlertDbaasExtended.label)
-            .should('be.visible');
-          cy.findByText(formatDate(mockAlertDbaasExtended.updated, { format: 'yyyy-MM-dd HH:mm' }))
-           .should('be.visible')
-           .should('have.text',formatDate(mockAlertDbaasExtended.updated, { format: 'yyyy-MM-dd HH:mm' }))
-  
-          cy.findByText(mockAlertDbaasExtended.created_by)
-          .should('be.visible')
-          .should('have.text',mockAlertDbaasExtended.created_by)
-     
-          // Click the action button
-          cy.get(`[data-qa-alert-action-cell="alert_${mockAlertDbaasExtended.id}"]`)
-            .find('button')
-           .should('be.visible')
-           .click({ force: true });
-   
-        });
-       
-        // Assert "Show Details" is visible
-        cy.get('[data-qa-action-menu-item="Show Details"]')
-          .should('be.visible')
-   
-          cy.get(`[data-qa-alert-action-cell="alert_${mockAlertDbaasExtended.id}"]`)
-          .find('button')
-         .should('be.visible')
-         .click({ force: true });
-
-
-
-     // Verify that the listing displays the Linode service type with the status set to 'Disabled'.
- 
-      cy.get(`[data-qa-alert-cell="${mockAlertLinodeExtended.id}"]`).within(() => {
-        // Assert each field inside the alert cell
-          cy.findByText(mockAlertLinodeExtended.service_type)
-            .should('be.visible')
-            .should('have.text',mockAlertLinodeExtended.service_type)
-  
-          cy.findByText(new RegExp(mockAlertLinodeExtended.status, 'i'))
-          .should('be.visible')
-          .should('have.text','Disabled')
-          cy.findByText(mockAlertLinodeExtended.label)
-            .should('be.visible');
-          cy.findByText(formatDate(mockAlertLinodeExtended.updated, { format: 'yyyy-MM-dd HH:mm' }))
-           .should('be.visible')
-           .should('have.text',formatDate(mockAlertLinodeExtended.updated, { format: 'yyyy-MM-dd HH:mm' }))
-  
-          cy.findByText(mockAlertLinodeExtended.created_by)
-          .should('be.visible')
-          .should('have.text',mockAlertLinodeExtended.created_by)
-     
-          // Click the action button
-          cy.get(`[data-qa-alert-action-cell="alert_${mockAlertLinodeExtended.id}"]`)
-            .find('button')
-           .should('be.visible')
-           .click({ force: true });
-   
-        });
-       
-        // Assert "Show Details" is visible
-        cy.get('[data-qa-action-menu-item="Show Details"]')
-          .should('be.visible')
-      
-       });
-   
+    // Validate the details of each mock alert
+    mockAlerts.forEach((alert) => {
+      checkAlertDetails(alert);
+      cy.get('[data-qa-action-menu-item="Show Details"]').should('be.visible');
+    });
   });
+});
